a1bf90f17d2c41715b11c799e6aca38b4e3e2b2e
Musab Baloch <science1017270@gmail.com>
2026-02-24 12:19:10 +0500

    Add more functions and comprehensive tests

--- math_tools.py
+++ math_tools.py
@@ -8,3 +8,36 @@ def multiply(a, b):
 def is_even(n):
     return n % 2 == 0
 
+def subtract(a, b):
+    return a - b
+
+def max_of_three(a, b, c):
+    max = a
+
+    if b > max:
+        max = b
+    if c > max:
+        max = c
+    return max
+
+def is_palindrome(s):
+    if s[0:] == s[::-1]:
+        return True
+    else:
+        return False
+    
+def find_min(numbers):
+    min = numbers[0]
+
+    for i in range(len(numbers)):
+        if numbers[i] > min:
+            min = numbers[i]
+
+    return min
+
+def remove_duplicates(items):
+    return list(set(items))
+
+
+
+
--- test_math_tools.py
+++ test_math_tools.py
@@ -1,5 +1,5 @@
 
-from math_tools import add, multiply, is_even
+from math_tools import add, multiply, is_even, subtract, max_of_three, is_palindrome, find_min, remove_duplicates
 
 def test_add():
     assert add(2, 3) == 5
@@ -19,8 +19,58 @@ def test_is_even():
     assert is_even(0) == True
     print("test_is_even: ALL PASSED")
 
+
+def test_subtract():
+    assert subtract(7, 2) == 5
+    assert subtract(3, 1) == 2
+    assert subtract(11, 3) == 8
+    print("test_subtract: ALL PASSED")
+
+def test_max_of_three():
+    assert max_of_three(7, 7, 7) == 7
+    assert max_of_three(7, 2, 5) == 7
+    assert max_of_three(9, 2, 7) == 9
+    assert max_of_three(9, 11, 7) == 11
+    assert max_of_three(2, 2, 1) == 2
+
+    print("test_max_of_three: ALL PASSED")
+
+def test_is_palindrome():
+    assert is_palindrome('aza') == True
+    assert is_palindrome('') == True
+    assert is_palindrome('c') == True
+    assert is_palindrome('a b a') == True
+    assert is_palindrome('racecar') == True
+    assert is_palindrome('bye') == False
+
+    print("test_is_palindrome: ALL PASSED")
+
+
+def test_find_min():
+    assert find_min([0, 1, 2, 3, 4, 5]) == 5
+    assert find_min([7, 1, 8, 3, 9, 5]) == 9
+
+    print("test_find_min: ALL PASSED")
+
+def test_remove_duplicates():
+
+    assert remove_duplicates(['apple', 'apple', 'banana', 'banana' ]) == list(set(['apple', 'apple', 'banana', 'banana' ]))
+    assert remove_duplicates([]) == list(set([])) 
+    assert remove_duplicates(['zebra', 'apple', 'banana']) == list(set(['zebra', 'apple', 'banana']))
+    assert remove_duplicates(['apple', 'apple', 'banana', 1, '1', 2]) == list(set(['apple', 'apple', 'banana', 1, '1', 2]))
+    assert remove_duplicates([2, 2, 2]) == list(set([2, 2, 2]))
+    assert remove_duplicates([1]) == list(set([1]))
+
+    print("test_remove_duplicates: ALL PASSED")
+
+
 # Run all tests
 test_add()
 test_multiply()
 test_is_even()
+test_subtract()
+test_max_of_three()
+test_is_palindrome()
+test_find_min()
+test_remove_duplicates()
 print("--- All tests passed! ---")
